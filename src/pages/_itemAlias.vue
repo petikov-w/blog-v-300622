b6e9eba243ef6c9c60648884b1ffa1c47f918cb7
krox2 <developer-krox@yandex.ru>
2022-07-01 00:24:35 +0300

    010722-0020 Начал страницу отдельного поста - корректировка 01

--- src/pages/_itemAlias.vue
+++ src/pages/_itemAlias.vue
@@ -1,14 +1,7 @@
 <template lang="pug">
 - var s_center="text-align: center;";
-
 .wrapper(style=s_center)
   h1 {{ item }}
-  //div(v-if="item" style=m_top)
-  //img(:src="item.img" :alt="item.title")
-  //h1.title {{ item.title }}
-  //p {{ item.descr }}
-  //cardStat(:stats="item.info")
-  //div(style=m_top)
   router-link(to="/" class="btn btnPrimary") Вернуться на главную
 </template>
 
@@ -21,14 +14,9 @@ export default {
     }
   },
   created() {
-
     console.log('======  ',this.$route)
     this.item = "http://api.blog.loc/posts" + this.$route.fullPath;
-
-
   },
-
-
 }
 </script>
 
