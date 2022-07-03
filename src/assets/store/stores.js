5733d56bde5eb93989b3d4aa289b8ffdb336f1a0
Petikov Vladimir <developer-krox@yandex.ru>
2022-07-03 18:25:35 +0300

    030722-1830 Продолжение работы с VUEX

--- src/assets/store/stores.js
+++ src/assets/store/stores.js
@@ -2,37 +2,89 @@ import {createStore} from "vuex";
 //import {ref} from "vue";
 import axios from "axios";
 
-export default createStore({
+export default createStore( {
         state: {
-            posts:  [];
-            /*const telefon = ref("79094282452");
-            const contentHeader=  {
-                logo: require('@/assets/images/logo-2.jpg'),
-                telefon: telefon.value,
-                subtitle: "Срочный вопрос"
-            }*/
-            /*const main_content = {
-                title: "Качественный газ для газгольдера",
-                subtitle: "Доставка в любое время дня и ночи вне зависимости от погодных условий"
-            };*/
+            posts:  [],
+            // title: "Всем Большой Приветик",
+            telefon: "79094282452"
+            // const contentHeader=  {
+            //     logo: require('@/assets/images/logo-2.jpg'),
+            //     telefon: telefon.value,
+            //     subtitle: "Срочный вопрос"
+            // }*/
+            // /*const main_content = {
+            //     title: "Качественный газ для газгольдера",
+            //     subtitle: "Доставка в любое время дня и ночи вне зависимости от погодных условий"
+            // };*/
 
 
-        },
-        mutations: {
-            SET_POSTS_TO_STATE: (state, posts) => state.posts=posts;
-        },
-        actions: {
-            GET_POSTS_FROM_API({commit}) {
-                return axios('http://api.blog.loc/posts',{ method: "GET"})
-                     .then((posts) => {
-                         commit(SET_POSTS_TO_STATE, posts);
-                     })
         },
         getters: {
-            POSTS(state) {
-                return state.posts;
+            // POSTS(state) {
+            //     return state.posts;
+            // },
+            getPosts: (state) => state.posts,
+            getTelefon: (state) => state.telefon
+
+            // TELEFON(state) {
+            //     return state.telefon;
+            // }
+        },
+        mutations: {
+            SET_POSTS_TO_STATE(state, posts) {
+                state.posts = posts
             }
-        }
-        }
-    }
-    )
+        },
+        actions: {
+           async GET_POSTS_FROM_API(context) {
+                // const data = await axios.get('http://api.blog.loc/posts')
+                // this.posts = data.data;
+                // console.log(this.posts)
+               // then(responce => {this.posts = responce.data
+               //     console.log(responce)
+               //     console.log(this.posts)}
+               // )
+
+
+               try {
+                   const data = await axios.get('http://api.blog.loc/posts');
+                   context.commit('SET_POSTS_TO_STATE', data.data)
+
+
+               }
+               catch (error) {
+                   alert(error)
+                   console.log(error)
+               }
+
+
+            } //end ---
+    } //actions end
+  }
+)
+
+
+
+/*GET_POSTS_FROM_API({commit}) {
+    return axios
+        .get('http://api.blog.loc/posts')
+        .then(responce => {this.posts = responce.data
+            console.log(responce)
+            console.log(this.posts)}
+        ).then((posts) => { commit('SET_POSTS_TO_STATE', posts);
+            return posts;
+            // axios('http://api.blog.loc/posts',{ method: "GET"})
+            //      .then((posts) => {
+            //          commit('SET_POSTS_TO_STATE', posts);
+            //          return posts;
+        })
+
+},*/
+
+// axios
+//     .get('http://api.blog.loc/posts')
+//     .then(responce => {this.posts = responce.data
+//         console.log(responce)
+//         console.log(this.posts)}
+//     )
+//     .catch(e => {this.errors.push(e)})
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 .wrapper
   .card-box
+    h1 {{ title }}
+    h1 {{ posts }}
     div(v-for="value in posts" :key="value.posts" )
       .card
         .title {{ value.title }}
@@ -10,35 +12,18 @@
 </template>
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
+import {useStore} from 'vuex'
+import {computed} from "vue";
+
 export default {
   name: "HomePage",
-  data() {
-    return {
-      posts: [],
-      errors: []
-    }
-  },
-  created() {
-    axios
-        .get('http://api.blog.loc/posts')
-        .then(responce => {this.posts = responce.data
-              console.log(responce)
-              console.log(this.posts)}
-        )
-        .catch(e => {this.errors.push(e)})
-  },
-  methods: {
-    link(value) {
-      return `http://api.blog.loc/posts/${value}`;
-    },
-    secWord(str, count) {
-        const adis = str.split(" ");
-        let result = '';
-        for(let i=0; i < count; i++) { result += adis[i]+" "; }
-        result += "...";
-    return result;
-    }
+  setup(){
+      const store = useStore();
+      const title = computed(() => store.getters.getTelefon);
+      const posts = computed(() => store.getters.getPosts);
+      return {title, posts}
+
   }
 }
 </script>
